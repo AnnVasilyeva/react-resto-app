c24c7220e6b07f3e84ecc17d3045936a7738d4fe
Ann Vasilyeva <skazkadreamanna@gmail.com>
2021-10-27 18:28:30 +0300

    Initial commit

--- src/components/hoc/with-resto-service.js
+++ src/components/hoc/with-resto-service.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import RestoServiceContext from '../resto-service-context';
 
-const WithRestoService = () => () => {
-    return 1;
+// WithRestoService компонент высшего порядка и принимает какой-то компонент (Wrapped) и все его аргументы (props). И этот компонент со всем его содержимым мы сразу рендерим внутри Consumer, тем самым избавляемся от лишнего повторения кода.
+const WithRestoService = () => (Wrapped) => {
+    return (props) => {
+        return (
+           <RestoServiceContext.Consumer>
+                {/* RestoService это контекст из RestoServiceContext*/}
+               {
+                   (RestoService) => {
+                       return <Wrapped {...props} Restoservice={RestoService}/>
+                   }
+               }
+           </RestoServiceContext.Consumer> 
+        )
+    }
 };
 
 export default WithRestoService;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ErrorBoundry from './components/error-boundry/error-boundry';
+import RestoService from './services/resto-service';
+import RestoSrviceContext from './components/resto-service-context';
+import store from './store';
 
 import './index.scss';
-
+const restoService = new RestoService;
 
 ReactDOM.render(
-    <App/>
+    <Provider store={store}>
+        <ErrorBoundry>
+            <RestoSrviceContext.Provider value={restoService}>
+                <Router>
+                    <App/>
+                </Router>
+            </RestoSrviceContext.Provider>
+        </ErrorBoundry>
+    </Provider>
     , document.getElementById('root'));
 
