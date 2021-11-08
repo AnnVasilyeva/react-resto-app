9593541915a11a609d010e7d4da067fb82537de9
Ann Vasilyeva <skazkadreamanna@gmail.com>
2021-11-08 11:26:58 +0300

    Add menu and cart

--- src/components/hoc/with-resto-service.js
+++ src/components/hoc/with-resto-service.js
@@ -2,14 +2,14 @@ import React from 'react';
 import RestoServiceContext from '../resto-service-context';
 
 // WithRestoService компонент высшего порядка и принимает какой-то компонент (Wrapped) и все его аргументы (props). И этот компонент со всем его содержимым мы сразу рендерим внутри Consumer, тем самым избавляемся от лишнего повторения кода.
+ /* RestoService это контекст из RestoServiceContext*/
 const WithRestoService = () => (Wrapped) => {
     return (props) => {
         return (
            <RestoServiceContext.Consumer>
-                {/* RestoService это контекст из RestoServiceContext*/}
                {
                    (RestoService) => {
-                       return <Wrapped {...props} Restoservice={RestoService}/>
+                       return <Wrapped {...props} RestoService={RestoService}/>
                    }
                }
            </RestoServiceContext.Consumer> 
--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,19 +1,68 @@
 import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
+import { connect } from 'react-redux';
+import WithRestoService from '../hoc';
+import {menuLoaded, menuRequested, addedToCart} from '../../actions';
+import Spinner from '../spinner'; 
 
 import './menu-list.scss';
 
 class MenuList extends Component {
+    componentDidMount() {
+        this.props.menuRequested();
+        
+        const {RestoService} = this.props;
+        RestoService.getMenuItems()
+        .then(res => this.props.menuLoaded(res));
+        // передаем данные из сервера в redux-store
+    }
 
     render() {
+        // получаем данные уже из redux-store
+        const {menuItems, loading, addedToCart} = this.props;
 
+        if(loading) {
+            return <Spinner/>
+        }
         return (
             <ul className="menu__list">
-                <MenuListItem/>
+                {
+                 menuItems.map(menuItem => {
+                    return <MenuListItem 
+                        key={menuItem.id} 
+                        menuItem={menuItem}
+                        onAddToCart={() => addedToCart(menuItem.id)}
+                        />
+                 })
+                }   
             </ul>
         )
     }
 };
 
+// говорим компоненту MenuList какие именно свойства из нашего state он будет получать для своей работы
+const mapStateToProps = (state) => {
+    return {
+       menuItems: state.menu,
+       loading: state.loading 
+    }
+};
 
-export default MenuList;
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         menuLoaded: (newMenu) => {
+//             dispatch(menuLoaded(newMenu))
+//         }
+//     }
+// }
+// или просто так:
+
+const mapDispatchToProps = {
+    menuLoaded, 
+    menuRequested,
+    addedToCart
+}
+
+// из компонента отправляем данные в redux-store с помощью WithRestoService
+// чтобы связать компонент MenuList с redux используем connect()
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
